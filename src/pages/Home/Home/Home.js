8ab9cafd793c0ee356d87f5500d0386b1ed75d99
Akzaber <zaberkhan611@gmail.com>
2023-03-22 12:16:57 +0600

    starting commit

---
+++ src/pages/Home/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Banner from '../Banner/Banner';
+import Newsection from '../NewSection/Newsection';
+
+const Home = () => {
+          return (
+                    <div className='mx-5 my-10'>
+                              <Banner></Banner>
+                              <Newsection></Newsection>
+                    </div>
+          );
+};
+
+export default Home;
